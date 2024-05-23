84ad9167353dfcefd03a5a9d2825bd93349d58ad
Sid <sid@Sid-2.local>
2024-05-23 09:32:25 -0400

    added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Author = Siddhartha Shrestha
+#Student id = 160706222
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
