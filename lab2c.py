979fc00839853e14bd159f9cdd51b63c72829be4
Sid <sid@Sid-2.local>
2024-05-23 09:30:09 -0400

    added lab2c.py

---
+++ lab2c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#Author = Siddhartha Shrestha
+#Student id = 160706222
+import sys
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+print('Hi ' + name +', you are ' + str(age) + ' years old.')
