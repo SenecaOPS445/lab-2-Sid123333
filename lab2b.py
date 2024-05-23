e2baf0fb41b427f950fd6497a7f1d0f1aafde504
Sid <sid@Sid-2.local>
2024-05-23 09:29:48 -0400

    added lab2b.py

---
+++ lab2b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#Author = Siddhartha Shrestha
+#Studwnt id = 160706222
+name = input("Name: ")
+age = input("Age: ")
+print('Hi ' + name +', you are ' + str(age) + ' years old.')
